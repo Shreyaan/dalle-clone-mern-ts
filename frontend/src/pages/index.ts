8afec8079a791592f0e7c6d7b235684e8719703c
shreyaan <Shreyaans41@gmail.com>
2023-01-26 15:25:31 +0530

    api url

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import CreatePost from "./CreatePost";
 
-const api_url = "http://localhost:3000/api/v1";
+const api_url = "https://dalle-clone-mern-ts-production.up.railway.app/api/v1";
 
 export { Home, CreatePost , api_url };
