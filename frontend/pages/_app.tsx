281ddd45620eee92c8fe4bfd14bee9f0fd078a31
shreyaan <Shreyaans41@gmail.com>
2023-01-26 23:51:00 +0530

    .

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -27,6 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)] ">
 
       <Component {...pageProps} />
+      
       </main>
     </>
   );
