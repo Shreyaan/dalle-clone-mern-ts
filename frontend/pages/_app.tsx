fd07de92a3dc995fbfb3fd7aad338549d3ed06fb
shreyaan <Shreyaans41@gmail.com>
2023-01-26 19:24:51 +0530

    shift done to nextjs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
--- frontend/src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import CreatePost from "./CreatePost";
-
-const api_url = "https://dalle-clone-mern-ts-production.up.railway.app/api/v1";
-
-export { Home, CreatePost , api_url };
